77e1137b27ffbc6423455e008b4d1eb751578841
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-02-22 21:47:09 +0500

    first commit

---
+++ a.py
@@ -0,0 +1,20 @@
+sum = 0
+
+for i in range (1 , 1000):
+    if i%3 == 0:
+            sum = sum + i
+
+    elif i%5 == 0:
+            sum = sum + i
+
+
+print(sum)
+
+
+
+
+#for x in range(1, 11):
+#    print(x)
+
+#if True:
+#    print("smth")
